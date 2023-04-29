1016ad0c83fab5e4f02d5cdbde7025e27e013369
Razib Mollick <razib.mollick@gmail.com>
2023-04-29 17:11:26 -0500

    Project Initital structure to follow MVVM-C Pattern

---
+++ Weather/Weather/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Rimo Tech  on 4/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
